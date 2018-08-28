9437790a6b0e6c75bf642cdd0a083ab8546ebdf8
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-28 13:41:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers) {
-  let firstAndSecond = []
-  firstAndSecond[0] = drivers[0]
-  firstAndSecond[1] = drivers[1]
-  return firstAndSecond
+  return drivers.slice(0,1)
 }
 
 const returnLastTwoDrivers = function (drivers) {
