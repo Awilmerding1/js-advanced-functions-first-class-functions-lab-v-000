b52b493b091362d47beaa2aa374aba43712dcdd2
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-28 13:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers) {
-  return drivers.filter(function (driver) {driver[0] && driver[1]})
+  return drivers.filter(function (driver) {return driver[0] && driver[1]})
 }
